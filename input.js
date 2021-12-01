5aba833dcbb10f22cc550c92881050061536193e
Arvind Sharma <mail.arvind.s@gmail.com>
2021-12-01 22:13:45 +0000

    Added the connection part with input

--- input.js
+++ input.js
@@ -1,6 +1,8 @@
+let connection;
 
+const setupInput = function(conn) {
 
-const setupInput = function() {
+    connection = conn; // to accept an object that lets interaction with server
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
@@ -11,6 +13,40 @@ const setupInput = function() {
             process.stdout.write('.');
             if (key === '\u0003') {
                 process.exit();
+            }
+
+            else if (key === 'w') {
+               setTimeout(() => {
+                connection.write("Move: up");    
+               }, 50);
+                   
+               
+            }
+
+            else if (key === 'a') {
+                setTimeout(() => {
+                    connection.write("Move: left");
+                }, 50);
+                    
+        
+            }
+
+            else if (key === 's') {
+                setTimeout(() => {
+                    connection.write("Move: down");
+                }, 50);
+                  
+              
+ 
+            }
+
+            else if (key === 'd') {
+                setTimeout(() => {
+                    connection.write("Move: right");
+                }, 50)
+                    
+              
+
             }
         });
         
--- play.js
+++ play.js
@@ -4,7 +4,7 @@ const { setupInput } = require('./input');
 
 
 console.log("Connecting ...");
-connect();
+const connection = connect();
 
 
-setupInput();
+setupInput(connection);
